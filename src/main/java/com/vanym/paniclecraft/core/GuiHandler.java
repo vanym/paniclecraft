d1fb1ed0abf07034117b71681abecd05fe84e9c8
VanyM <vanymdev@gmail.com>
2023-01-29 22:24:43 +0400

    refactor GuiHandler

--- src/main/java/com/vanym/paniclecraft/core/GuiHandler.java
+++ src/main/java/com/vanym/paniclecraft/core/GuiHandler.java
@@ -1,23 +1,10 @@
 package com.vanym.paniclecraft.core;
 
-import com.vanym.paniclecraft.client.gui.GuiChess;
-import com.vanym.paniclecraft.client.gui.GuiEditAdvSign;
-import com.vanym.paniclecraft.client.gui.container.GuiCannon;
-import com.vanym.paniclecraft.client.gui.container.GuiPalette;
-import com.vanym.paniclecraft.client.gui.container.GuiPortableCrafting;
-import com.vanym.paniclecraft.container.ContainerCannon;
-import com.vanym.paniclecraft.container.ContainerPalette;
-import com.vanym.paniclecraft.container.ContainerPortableWorkbench;
-import com.vanym.paniclecraft.tileentity.TileEntityAdvSign;
-import com.vanym.paniclecraft.tileentity.TileEntityCannon;
-import com.vanym.paniclecraft.tileentity.TileEntityChessDesk;
-
 import cpw.mods.fml.common.network.IGuiHandler;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
-public class GuiHandler implements IGuiHandler {
+public final class GuiHandler implements IGuiHandler {
     
     @Override
     public Object getServerGuiElement(
@@ -27,21 +14,7 @@ public class GuiHandler implements IGuiHandler {
             int x,
             int y,
             int z) {
-        if (ID == GUIs.PALETTE.ordinal()) {
-            return new ContainerPalette(player.inventory);
-        }
-        if (ID == GUIs.PORTABLEWORKBENCH.ordinal()) {
-            return new ContainerPortableWorkbench(player, world);
-        }
-        if (ID == GUIs.CANNON.ordinal()) {
-            TileEntity tile = world.getTileEntity(x, y, z);
-            if (tile instanceof TileEntityCannon) {
-                return new ContainerCannon(player.inventory, (TileEntityCannon)tile);
-            } else {
-                return null;
-            }
-        }
-        return null;
+        return GUIs.values()[ID].getServerGuiElement(ID, player, world, x, y, z);
     }
     
     @Override
@@ -52,37 +25,6 @@ public class GuiHandler implements IGuiHandler {
             int x,
             int y,
             int z) {
-        if (ID == GUIs.ADVSIGN.ordinal()) {
-            TileEntity tile = world.getTileEntity(x, y, z);
-            if (tile instanceof TileEntityAdvSign) {
-                return new GuiEditAdvSign((TileEntityAdvSign)tile);
-            } else {
-                return null;
-            }
-        }
-        if (ID == GUIs.PALETTE.ordinal()) {
-            return new GuiPalette(new ContainerPalette(player.inventory));
-        }
-        if (ID == GUIs.PORTABLEWORKBENCH.ordinal()) {
-            return new GuiPortableCrafting(player, world);
-        }
-        if (ID == GUIs.CANNON.ordinal()) {
-            TileEntity tile = world.getTileEntity(x, y, z);
-            if (tile instanceof TileEntityCannon) {
-                return new GuiCannon(player.inventory, (TileEntityCannon)tile);
-            } else {
-                return null;
-            }
-        }
-        if (ID == GUIs.CHESS.ordinal()) {
-            TileEntity tile = world.getTileEntity(x, y, z);
-            if (tile instanceof TileEntityChessDesk) {
-                return new GuiChess((TileEntityChessDesk)tile);
-            } else {
-                return null;
-            }
-        }
-        return null;
+        return GUIs.values()[ID].getClientGuiElement(ID, player, world, x, y, z);
     }
-    
 }
