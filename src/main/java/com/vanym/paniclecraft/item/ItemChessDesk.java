aba341727044a36270002dd85c7505d0b8818c80
VanyM <vanymdev@gmail.com>
2023-03-08 09:49:59 +0400

    add ChessDesk item tooltips

--- src/main/java/com/vanym/paniclecraft/item/ItemChessDesk.java
+++ src/main/java/com/vanym/paniclecraft/item/ItemChessDesk.java
@@ -1,6 +1,10 @@
 package com.vanym.paniclecraft.item;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import com.vanym.paniclecraft.Core;
 import com.vanym.paniclecraft.tileentity.TileEntityChessDesk;
@@ -51,12 +55,45 @@ public class ItemChessDesk extends ItemBlock {
             EntityPlayer player,
             List list,
             boolean advancedItemTooltips) {
-        if (GuiScreen.isShiftKeyDown() || advancedItemTooltips) {
-            if (stack.hasTagCompound()) {
-                NBTTagCompound tag = stack.getTagCompound();
-                if (tag.hasKey(TAG_MOVES)) {
-                    list.add(StatCollector.translateToLocal("text.chessGameHaveSave"));
+        if (!stack.hasTagCompound()) {
+            return;
+        }
+        NBTTagCompound tag = stack.getTagCompound();
+        if (tag.hasKey(TAG_MOVES)) {
+            NBTTagList movesTag = tag.getTagList(TAG_MOVES, 10);
+            list.add(StatCollector.translateToLocalFormatted("item.chessDesk.moves",
+                                                             movesTag.tagCount()));
+            if (GuiScreen.isShiftKeyDown()) {
+                Map<NBTTagCompound, Integer> white = new HashMap<>(), black = new HashMap<>();
+                for (int i = 0; i < movesTag.tagCount(); ++i) {
+                    NBTTagCompound moveTag = movesTag.getCompoundTagAt(i);
+                    if (!moveTag.hasKey(TileEntityChessDesk.TAG_PLAYER, 10)) {
+                        continue;
+                    }
+                    NBTTagCompound playerTag =
+                            moveTag.getCompoundTag(TileEntityChessDesk.TAG_PLAYER);
+                    Map<NBTTagCompound, Integer> map = (i % 2 == 0) ? white : black;
+                    map.put(playerTag, map.getOrDefault(playerTag, 0) + 1);
                 }
+                Stream.of(true, false).forEach(side-> {
+                    Map<NBTTagCompound, Integer> map = side ? white : black;
+                    boolean many = map.size() > 1;
+                    String translate =
+                            String.format("item.chessDesk.player.%s.%s",
+                                          side ? "white" : "black", many ? "many" : "one");
+                    map.entrySet()
+                       .stream()
+                       .sorted(Comparator.comparing(Map.Entry<NBTTagCompound, Integer>::getValue)
+                                         .reversed())
+                       .map(e-> {
+                           String name = e.getKey().getString(TileEntityChessDesk.TAG_PLAYERNAME);
+                           return StatCollector.translateToLocalFormatted(translate,
+                                                                          many ? new Object[]{name,
+                                                                                              e.getValue()}
+                                                                               : new Object[]{name});
+                       })
+                       .forEach(list::add);
+                });
             }
         }
     }
