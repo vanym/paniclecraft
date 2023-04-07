93a8e81a4ad45243e06397526addcf893221d7c5
VanyM <vanymdev@gmail.com>
2023-04-07 18:31:47 +0400

    upgrade ChessDesk tile renderer

--- src/main/java/com/vanym/paniclecraft/client/renderer/tileentity/TileEntityChessDeskRenderer.java
+++ src/main/java/com/vanym/paniclecraft/client/renderer/tileentity/TileEntityChessDeskRenderer.java
@@ -1,7 +1,6 @@
 package com.vanym.paniclecraft.client.renderer.tileentity;
 
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL12;
 
 import com.vanym.paniclecraft.DEF;
 import com.vanym.paniclecraft.client.renderer.model.ModelChessBishop;
@@ -14,6 +13,7 @@ import com.vanym.paniclecraft.client.renderer.model.ModelChessRook;
 import com.vanym.paniclecraft.core.component.deskgame.ChessGame;
 import com.vanym.paniclecraft.tileentity.TileEntityChessDesk;
 
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
@@ -46,33 +46,44 @@ public class TileEntityChessDeskRenderer extends TileEntitySpecialRenderer<TileE
             float partialTicks,
             int destroyStage,
             float alpha) {
-        GL11.glPushMatrix();
-        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        GL11.glTranslatef((float)x + 0.5F, (float)y + 0.5F, (float)z + 0.5F);
-        GL11.glRotatef(180.0F, 1.0F, 0.0F, 0.0F);
-        GL11.glTranslatef(0.0F, 0.5F, 0.0F);
-        GL11.glRotatef(90.0F * (tileCD.getWorld() == null ? 0 : tileCD.getBlockMetadata()),
-                       0.0F, 1.0F, 0.0F);
+        GlStateManager.pushMatrix();
+        GlStateManager.enableRescaleNormal();
+        GlStateManager.translate((float)x + 0.5F, (float)y + 0.5F, (float)z + 0.5F);
+        GlStateManager.rotate(180.0F, 1.0F, 0.0F, 0.0F);
+        GlStateManager.translate(0.0F, 0.5F, 0.0F);
+        if (tileCD.hasWorld()) {
+            GlStateManager.rotate(90.0F * tileCD.getBlockMetadata(), 0.0F, 1.0F, 0.0F);
+        }
         float scale = 0.0625F;
-        this.bindTexture(TEXTURE);
+        if (destroyStage >= 0) {
+            this.bindTexture(DESTROY_STAGES[destroyStage]);
+            GlStateManager.matrixMode(GL11.GL_TEXTURE);
+            GlStateManager.pushMatrix();
+            GlStateManager.scale(4.0F, 2.0F, 1.0F);
+            GlStateManager.translate(0.0625F, 0.0625F, 0.0625F);
+            GlStateManager.matrixMode(GL11.GL_MODELVIEW);
+        } else {
+            this.bindTexture(TEXTURE);
+        }
         this.desk.render(scale);
-        GL11.glTranslatef(0.5F - 0.0625F, -0.25F + 0.05F, 0.5F - 0.0625F);
+        GlStateManager.translate(0.5F - 0.0625F, -0.25F + 0.05F, 0.5F - 0.0625F);
         float scalePiece = 0.25F;
         ChessGame game = tileCD.getGame();
-        for (int i = 0; i < game.size(); ++i) {
+        int size = destroyStage < 0 ? game.size() : 0;
+        for (int i = 0; i < size; ++i) {
             int px = i % 8;
             int py = i / 8;
             byte piece = game.getPiece(i);
             byte pieceA = (byte)Math.abs(piece);
-            GL11.glPushMatrix();
-            GL11.glTranslatef(-0.125F * px, 0.0F, -0.125F * py);
+            GlStateManager.pushMatrix();
+            GlStateManager.translate(-0.125F * px, 0.0F, -0.125F * py);
             if (piece > 0) {
                 this.bindTexture(TEXTURE_W);
             } else if (piece < 0) {
-                GL11.glRotatef(180.0F, 0.0F, 1.0F, 0.0F);
+                GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
                 this.bindTexture(TEXTURE_B);
             }
-            GL11.glScalef(scale, scale, scale);
+            GlStateManager.scale(scale, scale, scale);
             if (pieceA == ChessGame.PAWN) {
                 this.pawn.render(scalePiece);
             } else if (pieceA == ChessGame.BISHOP) {
@@ -86,8 +97,13 @@ public class TileEntityChessDeskRenderer extends TileEntitySpecialRenderer<TileE
             } else if (pieceA == ChessGame.KING || pieceA == ChessGame.KING_UNMOVED) {
                 this.king.render(scalePiece);
             }
-            GL11.glPopMatrix();
+            GlStateManager.popMatrix();
         }
-        GL11.glPopMatrix();
+        GlStateManager.popMatrix();
+        if (destroyStage >= 0) {
+            GlStateManager.matrixMode(GL11.GL_TEXTURE);
+            GlStateManager.popMatrix();
+            GlStateManager.matrixMode(GL11.GL_MODELVIEW);
+        }
     }
 }
