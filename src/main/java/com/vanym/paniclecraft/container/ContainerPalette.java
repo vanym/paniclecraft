178eb83bdf35f943247758b222f2fe9b97fd6341
VanyM <vanymdev@gmail.com>
2023-04-08 13:37:48 +0400

    fix ContainerPalette.transferStackInSlot

--- src/main/java/com/vanym/paniclecraft/container/ContainerPalette.java
+++ src/main/java/com/vanym/paniclecraft/container/ContainerPalette.java
@@ -61,6 +61,8 @@ public class ContainerPalette extends ContainerBase implements IInvBasic {
         
         if (slot != null && slot.getHasStack()) {
             ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+            
             if (slotNum == 0) {
                 if (!this.mergeItemStack(itemstack1, 1, 37, true)) {
                     return null;
@@ -81,6 +83,12 @@ public class ContainerPalette extends ContainerBase implements IInvBasic {
             } else {
                 slot.onSlotChanged();
             }
+            
+            if (itemstack1.stackSize == itemstack.stackSize) {
+                return null;
+            }
+            
+            slot.onPickupFromSlot(player, itemstack1);
         }
         
         return itemstack;
