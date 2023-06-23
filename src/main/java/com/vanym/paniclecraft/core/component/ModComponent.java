07245b8ad9bf5d456eb2a60963f033caea12d859
VanyM <vanymdev@gmail.com>
2023-06-23 17:24:44 +0400

    upgrade IModComponent, ModComponent

--- src/main/java/com/vanym/paniclecraft/core/component/ModComponent.java
+++ src/main/java/com/vanym/paniclecraft/core/component/ModComponent.java
@@ -13,23 +13,10 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.Stream.Builder;
 
-import org.apache.commons.lang3.tuple.ImmutablePair;
-
-import com.vanym.paniclecraft.DEF;
-import com.vanym.paniclecraft.block.IWithCustomStateMapper;
-import com.vanym.paniclecraft.item.IWithSubtypes;
-
 import net.minecraft.block.Block;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public abstract class ModComponent implements IModComponent {
     
@@ -43,43 +30,6 @@ public abstract class ModComponent implements IModComponent {
         this.getBlocks().forEach(e.getRegistry()::register);
     }
     
-    @SubscribeEvent
-    public void registerRecipes(RegistryEvent.Register<IRecipe> e) {
-        this.getRecipes().forEach(e.getRegistry()::register);
-    }
-    
-    @SubscribeEvent
-    @SideOnly(Side.CLIENT)
-    public void registerModels(ModelRegistryEvent event) {
-        this.getBlocks().forEach(this::registerModel);
-        this.getItems().forEach(this::registerModel);
-    }
-    
-    @SideOnly(Side.CLIENT)
-    protected void registerModel(Block block) {
-        if (block instanceof IWithCustomStateMapper) {
-            IWithCustomStateMapper mapper = (IWithCustomStateMapper)block;
-            ModelLoader.setCustomStateMapper(block, mapper.getStateMapper());
-        }
-    }
-    
-    @SideOnly(Side.CLIENT)
-    protected void registerModel(Item item) {
-        Stream<ImmutablePair<Integer, ResourceLocation>> st;
-        if (item instanceof IWithSubtypes) {
-            IWithSubtypes ty = (IWithSubtypes)item;
-            st = ty.getSubtypes()
-                   .entrySet()
-                   .stream()
-                   .map(e->ImmutablePair.of(e.getKey(), e.getValue()))
-                   .map(e->ImmutablePair.of(e.left, new ResourceLocation(DEF.MOD_ID, e.right)));
-        } else {
-            st = Stream.of(ImmutablePair.of(0, item.getRegistryName()));
-        }
-        st.map(e->ImmutablePair.of(e.left, new ModelResourceLocation(e.right, "inventory")))
-          .forEach(e->ModelLoader.setCustomModelResourceLocation(item, e.left, e.right));
-    }
-    
     @Override
     public List<Item> getItems() {
         return this.getObjects(Item.class);
@@ -87,10 +37,6 @@ public abstract class ModComponent implements IModComponent {
     
     public List<Block> getBlocks() {
         return this.getObjects(Block.class);
-    }
-    
-    public List<IRecipe> getRecipes() {
-        return this.getObjects(IRecipe.class);
     }
     
     protected final <T> List<T> getObjects(Class<T> clazz) {
