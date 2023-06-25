3da7ebdcdf43d5ee6d904a050ac2bb8116154463
VanyM <vanymdev@gmail.com>
2023-06-25 13:09:38 +0400

    upgrade proxy

--- src/main/java/com/vanym/paniclecraft/core/IProxy.java
+++ src/main/java/com/vanym/paniclecraft/core/IProxy.java
@@ -1,11 +1,5 @@
 package com.vanym.paniclecraft.core;
 
 public interface IProxy {
-    public void preInit(ModConfig config);
     
-    public void init(ModConfig config);
-    
-    public void postInit(ModConfig config);
-    
-    public void configChanged(ModConfig config);
 }
