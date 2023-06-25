a5663e1ae7ce86a84e4d39c94a100f271058155b
VanyM <vanymdev@gmail.com>
2023-06-25 16:24:25 +0400

    upgrade models

--- src/main/java/com/vanym/paniclecraft/client/renderer/model/ModelChessBishop.java
+++ src/main/java/com/vanym/paniclecraft/client/renderer/model/ModelChessBishop.java
@@ -1,43 +1,43 @@
 package com.vanym.paniclecraft.client.renderer.model;
 
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelRenderer;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.entity.model.RendererModel;
+import net.minecraft.client.renderer.model.Model;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-@SideOnly(Side.CLIENT)
-public class ModelChessBishop extends ModelBase {
-    ModelRenderer body1;
-    ModelRenderer body2;
-    ModelRenderer body3;
-    ModelRenderer body4;
-    ModelRenderer body5;
+@OnlyIn(Dist.CLIENT)
+public class ModelChessBishop extends Model {
+    RendererModel body1;
+    RendererModel body2;
+    RendererModel body3;
+    RendererModel body4;
+    RendererModel body5;
     
     public ModelChessBishop() {
         textureWidth = 32;
         textureHeight = 16;
         
-        body1 = new ModelRenderer(this, 0, 0);
+        body1 = new RendererModel(this, 0, 0);
         body1.addBox(-2.5F, 0F, -2.5F, 5, 1, 5);
         body1.setRotationPoint(0F, 0F, 0F);
         body1.setTextureSize(32, 16);
         body1.mirror = true;
-        body2 = new ModelRenderer(this, 12, 6);
+        body2 = new RendererModel(this, 12, 6);
         body2.addBox(-1.5F, 0F, -1.5F, 3, 1, 3);
         body2.setRotationPoint(-9.992007E-15F, -1F, 0F);
         body2.setTextureSize(32, 16);
         body2.mirror = true;
-        body3 = new ModelRenderer(this, 0, 6);
+        body3 = new RendererModel(this, 0, 6);
         body3.addBox(-0.5F, -3F, -0.5F, 1, 4, 1);
         body3.setRotationPoint(0F, -2F, 0F);
         body3.setTextureSize(32, 16);
         body3.mirror = true;
-        body4 = new ModelRenderer(this, 4, 6);
+        body4 = new RendererModel(this, 4, 6);
         body4.addBox(-1F, -1F, -1.033333F, 2, 3, 2);
         body4.setRotationPoint(0F, -7F, 0F);
         body4.setTextureSize(32, 16);
         body4.mirror = true;
-        body5 = new ModelRenderer(this, 12, 10);
+        body5 = new RendererModel(this, 12, 10);
         body5.addBox(-0.5F, 0F, -0.5F, 1, 1, 1);
         body5.setRotationPoint(0F, -9F, 0F);
         body5.setTextureSize(32, 16);
--- src/main/java/com/vanym/paniclecraft/client/renderer/model/ModelChessDesk.java
+++ src/main/java/com/vanym/paniclecraft/client/renderer/model/ModelChessDesk.java
@@ -1,19 +1,19 @@
 package com.vanym.paniclecraft.client.renderer.model;
 
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelRenderer;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.entity.model.RendererModel;
+import net.minecraft.client.renderer.model.Model;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-@SideOnly(Side.CLIENT)
-public class ModelChessDesk extends ModelBase {
-    ModelRenderer Shape1;
+@OnlyIn(Dist.CLIENT)
+public class ModelChessDesk extends Model {
+    RendererModel Shape1;
     
     public ModelChessDesk() {
         textureWidth = 64;
         textureHeight = 32;
         
-        Shape1 = new ModelRenderer(this, 0, 0);
+        Shape1 = new RendererModel(this, 0, 0);
         Shape1.addBox(-8F, -3F, -8F, 16, 3, 16);
         Shape1.setRotationPoint(0F, 0F, 0F);
         Shape1.setTextureSize(64, 32);
