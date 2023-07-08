c5ef7b6d542e5afa868e5fdab10f644418b32c5d
VanyM <vanymdev@gmail.com>
2023-07-08 16:49:46 +0400

    upgrade jei plugin

--- src/main/java/com/vanym/paniclecraft/plugins/jei/JEIMod3Config.java
+++ src/main/java/com/vanym/paniclecraft/plugins/jei/JEIMod3Config.java
@@ -6,30 +6,43 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.vanym.paniclecraft.Core;
-import com.vanym.paniclecraft.client.gui.container.GuiPortableCrafting;
+import com.vanym.paniclecraft.DEF;
 import com.vanym.paniclecraft.container.ContainerPortableWorkbench;
 
 import mezz.jei.api.IModPlugin;
-import mezz.jei.api.IModRegistry;
-import mezz.jei.api.JEIPlugin;
-import mezz.jei.api.recipe.IRecipeWrapper;
-import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.registration.IRecipeTransferRegistration;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 
-@JEIPlugin
+@JeiPlugin
 public class JEIMod3Config implements IModPlugin {
     
     @Override
-    public void register(IModRegistry registry) {
+    public void registerRecipes(IRecipeRegistration registry) {
         if (Core.instance.painting.isEnabled()) {
-            this.registerPainting(registry);
-        }
-        if (Core.instance.portableworkbench.isEnabled()) {
-            this.registerPortableWorkbench(registry);
+            this.registerPaintingRecipes(registry);
         }
     }
     
-    protected void registerPainting(IModRegistry registry) {
+    @Override
+    public void registerRecipeTransferHandlers(IRecipeTransferRegistration registry) {
+        if (Core.instance.portableworkbench.isEnabled()) {
+            this.registerPortableWorkbenchRecipeTransferHandlers(registry);
+        }
+    }
+    
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        if (Core.instance.portableworkbench.isEnabled()) {
+            this.registerPortableWorkbenchRecipeCatalysts(registration);
+        }
+    }
+    
+    protected void registerPaintingRecipes(IRecipeRegistration registry) {
         ItemStack painting = new ItemStack(Core.instance.painting.itemPainting);
         List<ItemStack> left = Arrays.asList(painting);
         List<ItemStack> right = Collections.nCopies(left.size(), painting);
@@ -37,20 +50,26 @@ public class JEIMod3Config implements IModPlugin {
                                      .map(ItemStack::copy)
                                      .peek(stack->stack.setCount(2))
                                      .collect(Collectors.toList());
-        IRecipeWrapper recipe = registry.getJeiHelpers()
-                                        .getVanillaRecipeFactory()
-                                        .createAnvilRecipe(left, right, output);
+        Object recipe = registry.getVanillaRecipeFactory()
+                                .createAnvilRecipe(left, right, output);
         registry.addRecipes(Arrays.asList(recipe), VanillaRecipeCategoryUid.ANVIL);
     }
     
-    protected void registerPortableWorkbench(IModRegistry registry) {
-        registry.getRecipeTransferRegistry()
-                .addRecipeTransferHandler(ContainerPortableWorkbench.class,
+    protected void registerPortableWorkbenchRecipeTransferHandlers(
+            IRecipeTransferRegistration registry) {
+        registry.addRecipeTransferHandler(ContainerPortableWorkbench.class,
                                           VanillaRecipeCategoryUid.CRAFTING,
                                           1, 9, 10, 36);
-        registry.addRecipeClickArea(GuiPortableCrafting.class, 88, 32, 28, 23,
-                                    VanillaRecipeCategoryUid.CRAFTING);
+    }
+    
+    protected void registerPortableWorkbenchRecipeCatalysts(
+            IRecipeCatalystRegistration registry) {
         registry.addRecipeCatalyst(new ItemStack(Core.instance.portableworkbench.itemWorkbench),
                                    VanillaRecipeCategoryUid.CRAFTING);
+    }
+    
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(DEF.MOD_ID, "jei");
     }
 }
