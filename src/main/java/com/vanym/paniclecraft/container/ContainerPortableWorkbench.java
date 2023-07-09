da2c9637fec9dea1291594a0fd5e6da4f8a3db55
VanyM <vanymdev@gmail.com>
2023-07-09 17:10:06 +0400

    fix ContainerPortableWorkbench

--- src/main/java/com/vanym/paniclecraft/container/ContainerPortableWorkbench.java
+++ src/main/java/com/vanym/paniclecraft/container/ContainerPortableWorkbench.java
@@ -1,18 +1,22 @@
 package com.vanym.paniclecraft.container;
 
+import com.vanym.paniclecraft.Core;
 import com.vanym.paniclecraft.container.slot.SlotPortableCrafting;
 import com.vanym.paniclecraft.item.ItemWorkbench;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.inventory.container.WorkbenchContainer;
 import net.minecraft.util.Hand;
+import net.minecraft.util.IWorldPosCallable;
 
 public class ContainerPortableWorkbench extends WorkbenchContainer {
     
     public ContainerPortableWorkbench(int id, PlayerInventory inventory) {
-        super(id, inventory);
+        super(id, inventory, IWorldPosCallable.of(inventory.player.world,
+                                                  inventory.player.getPosition()));
         Slot original = (Slot)this.inventorySlots.get(0);
         SlotPortableCrafting slot = new SlotPortableCrafting(
                 inventory.player,
@@ -22,6 +26,11 @@ public class ContainerPortableWorkbench extends WorkbenchContainer {
                 original.xPos,
                 original.yPos);
         this.inventorySlots.set(0, slot);
+    }
+    
+    @Override
+    public ContainerType<?> getType() {
+        return Core.instance.portableworkbench.containerPortableWorkbench;
     }
     
     @Override
