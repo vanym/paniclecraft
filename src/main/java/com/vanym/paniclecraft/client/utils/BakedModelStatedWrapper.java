395efdca4e5bbb3c2019aaf53ae19f53eea0ffc7
VanyM <vanymdev@gmail.com>
2023-07-13 00:32:15 +0400

    add second getQuads to BakedModelStatedWrapper

--- src/main/java/com/vanym/paniclecraft/client/utils/BakedModelStatedWrapper.java
+++ src/main/java/com/vanym/paniclecraft/client/utils/BakedModelStatedWrapper.java
@@ -9,7 +9,10 @@ import net.minecraft.block.BlockState;
 import net.minecraft.client.renderer.model.BakedQuad;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IEnviromentBlockReader;
 import net.minecraftforge.client.model.BakedModelWrapper;
+import net.minecraftforge.client.model.data.IModelData;
 
 public class BakedModelStatedWrapper<T extends IBakedModel> extends BakedModelWrapper<T> {
     
@@ -29,7 +32,25 @@ public class BakedModelStatedWrapper<T extends IBakedModel> extends BakedModelWr
     }
     
     @Override
+    public List<BakedQuad> getQuads(
+            @Nullable BlockState state,
+            @Nullable Direction side,
+            Random rand,
+            IModelData extraData) {
+        return super.getQuads(this.state, side, rand, extraData);
+    }
+    
+    @Override
     public boolean isAmbientOcclusion(BlockState state) {
         return super.isAmbientOcclusion(this.state);
+    }
+    
+    @Override
+    public IModelData getModelData(
+            IEnviromentBlockReader world,
+            BlockPos pos,
+            BlockState state,
+            IModelData tileData) {
+        return tileData;
     }
 }
