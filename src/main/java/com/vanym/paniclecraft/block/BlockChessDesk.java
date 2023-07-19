3ce66cd73dc55424c5c2508b152b420ac4e0f181
VanyM <vanymdev@gmail.com>
2023-07-19 12:41:52 +0400

    add waterlogged to chess desk

--- src/main/java/com/vanym/paniclecraft/block/BlockChessDesk.java
+++ src/main/java/com/vanym/paniclecraft/block/BlockChessDesk.java
@@ -11,15 +11,20 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
+import net.minecraft.block.IWaterLoggable;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.IFluidState;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.Direction;
@@ -31,14 +36,16 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.common.thread.EffectiveSide;
 
-public class BlockChessDesk extends HorizontalBlock {
+public class BlockChessDesk extends HorizontalBlock implements IWaterLoggable {
     
     public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     
     protected static final VoxelShape CHESS_DESK_SHAPE =
             VoxelShapes.create(0.0D, 0.0D, 0.0D, 1.0D, 3.0D / 16.0D, 1.0D);
@@ -49,7 +56,9 @@ public class BlockChessDesk extends HorizontalBlock {
                               .hardnessAndResistance(0.5F)
                               .noDrops());
         this.setRegistryName("chess_desk");
-        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
+        this.setDefaultState(this.stateContainer.getBaseState()
+                                                .with(FACING, Direction.NORTH)
+                                                .with(WATERLOGGED, false));
     }
     
     @Override
@@ -64,12 +73,39 @@ public class BlockChessDesk extends HorizontalBlock {
     
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, WATERLOGGED);
+    }
+    
+    @Override
+    @SuppressWarnings("deprecation")
+    public IFluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false)
+                                      : super.getFluidState(state);
     }
     
     @Override
     public BlockState getStateForPlacement(BlockItemUseContext context) {
-        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing());
+        IFluidState fluidstate = context.getWorld().getFluidState(context.getPos());
+        return this.getDefaultState()
+                   .with(FACING, context.getPlacementHorizontalFacing())
+                   .with(WATERLOGGED, fluidstate.getFluid() == Fluids.WATER);
+    }
+    
+    @Override
+    @SuppressWarnings("deprecation")
+    public BlockState updatePostPlacement(
+            BlockState state,
+            Direction facing,
+            BlockState facingState,
+            IWorld world,
+            BlockPos currentPos,
+            BlockPos facingPos) {
+        if (state.get(WATERLOGGED)) {
+            world.getPendingFluidTicks()
+                 .scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return super.updatePostPlacement(state, facing, facingState, world, currentPos,
+                                         facingPos);
     }
     
     @Override
