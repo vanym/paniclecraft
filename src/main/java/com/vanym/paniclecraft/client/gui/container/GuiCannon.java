838a32916ce3b7d6ec5f098bdc5660783a247032
VanyM <vanymdev@gmail.com>
2023-09-28 15:54:01 +0400

    add slider strict rotation tooltips

--- src/main/java/com/vanym/paniclecraft/client/gui/container/GuiCannon.java
+++ src/main/java/com/vanym/paniclecraft/client/gui/container/GuiCannon.java
@@ -122,6 +122,18 @@ public class GuiCannon extends GuiContainer {
         this.fontRendererObj.drawString(strengthString, 30, 28, 0x404040);
         double strength = this.container.cannon.getStrength();
         this.fontRendererObj.drawString(String.format("%.4f", strength), 30, 38, 0x404040);
+        if (this.isRotating()) {
+            String tooltipKey = GuiScreen.isShiftKeyDown() ? "gui.%s.cannon.slider_unshift_tooltip"
+                                                           : "gui.%s.cannon.slider_shift_tooltip";
+            this.drawCenteredString(this.fontRendererObj,
+                                    I18n.format(String.format(tooltipKey, DEF.MOD_ID)),
+                                    this.xSize / 2, this.ySize + 6, 0xd0d0d0);
+        }
+    }
+    
+    protected boolean isRotating() {
+        return Stream.of(this.sliderDir, this.sliderHeight, this.sliderStrength)
+                     .anyMatch(GuiCircularSlider::isPressed);
     }
     
     @Override
