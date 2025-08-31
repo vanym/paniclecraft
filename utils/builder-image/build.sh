301af4960a83003f813f77d6695d4a0c2755ee57
VanyM <vanymdev@gmail.com>
2025-08-31 18:14:57 +0400

    add builder image

---
+++ utils/builder-image/build.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+set -e
+
+TAG="2.7-1"
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+ROOT="$DIR"/../..
+
+mkdir -p .gradle build
+chown --reference=. .gradle build
+docker run --rm -it \
+    --network none \
+    -v "$ROOT:/root/src:ro" \
+    -w /root/src \
+    --tmpfs /root/src/build \
+    -v "$ROOT/out:/root/src/build/libs:rw" \
+    --tmpfs /root/src/.gradle \
+    ghcr.io/vanym/paniclecraft/builder:"$TAG" ./gradlew --offline jar --no-daemon
+chown --reference=. -R "$ROOT/out"
