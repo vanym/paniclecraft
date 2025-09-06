9471802400358c04f68ca659e909657c11f5eca1
VanyM <vanymdev@gmail.com>
2025-09-06 18:56:33 +0400

    upgrade models

--- src/main/java/com/vanym/paniclecraft/client/renderer/model/ModelChessRook.java
+++ src/main/java/com/vanym/paniclecraft/client/renderer/model/ModelChessRook.java
@@ -1,96 +1,101 @@
 package com.vanym.paniclecraft.client.renderer.model;
 
-import net.minecraft.client.renderer.entity.model.RendererModel;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.model.Model;
+import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class ModelChessRook extends Model {
-    RendererModel body1;
-    RendererModel body2;
-    RendererModel body3;
-    RendererModel pike1;
-    RendererModel pike2;
-    RendererModel pike3;
-    RendererModel pike4;
-    RendererModel pike5;
-    RendererModel pike6;
-    RendererModel pike7;
-    RendererModel pike8;
+    ModelRenderer body1;
+    ModelRenderer body2;
+    ModelRenderer body3;
+    ModelRenderer pike1;
+    ModelRenderer pike2;
+    ModelRenderer pike3;
+    ModelRenderer pike4;
+    ModelRenderer pike5;
+    ModelRenderer pike6;
+    ModelRenderer pike7;
+    ModelRenderer pike8;
     
     public ModelChessRook() {
+        super(RenderType::entityCutoutNoCull);
         texWidth = 32;
         texHeight = 16;
         
-        body1 = new RendererModel(this, 0, 0);
+        body1 = new ModelRenderer(this, 0, 0);
         body1.addBox(-2.5F, 0F, -2.5F, 5, 1, 5);
         body1.setPos(0F, 0F, 0F);
         body1.setTexSize(32, 16);
         body1.mirror = true;
-        body2 = new RendererModel(this, 20, 0);
+        body2 = new ModelRenderer(this, 20, 0);
         body2.addBox(-1.5F, 0F, -1.5F, 3, 4, 3);
         body2.setPos(-9.992007E-15F, -4F, 0F);
         body2.setTexSize(32, 16);
         body2.mirror = true;
-        body3 = new RendererModel(this, 0, 6);
+        body3 = new ModelRenderer(this, 0, 6);
         body3.addBox(-2.5F, 0F, -2.5F, 5, 1, 5);
         body3.setPos(0F, -5F, 0F);
         body3.setTexSize(32, 16);
         body3.mirror = true;
-        pike1 = new RendererModel(this, 20, 7);
+        pike1 = new ModelRenderer(this, 20, 7);
         pike1.addBox(1.5F, 0F, 1.5F, 1, 1, 1);
         pike1.setPos(0F, -6F, 0F);
         pike1.setTexSize(32, 16);
         pike1.mirror = true;
-        pike2 = new RendererModel(this, 20, 7);
+        pike2 = new ModelRenderer(this, 20, 7);
         pike2.addBox(-0.5F, 0F, 1.5F, 1, 1, 1);
         pike2.setPos(0F, -6F, 0F);
         pike2.setTexSize(32, 16);
         pike2.mirror = true;
-        pike3 = new RendererModel(this, 20, 7);
+        pike3 = new ModelRenderer(this, 20, 7);
         pike3.addBox(-2.5F, 0F, 1.5F, 1, 1, 1);
         pike3.setPos(0F, -6F, 0F);
         pike3.setTexSize(32, 16);
         pike3.mirror = true;
-        pike4 = new RendererModel(this, 20, 7);
+        pike4 = new ModelRenderer(this, 20, 7);
         pike4.addBox(-2.5F, 0F, -0.5F, 1, 1, 1);
         pike4.setPos(0F, -6F, 0F);
         pike4.setTexSize(32, 16);
         pike4.mirror = true;
-        pike5 = new RendererModel(this, 20, 7);
+        pike5 = new ModelRenderer(this, 20, 7);
         pike5.addBox(-2.5F, 0F, -2.5F, 1, 1, 1);
         pike5.setPos(0F, -6F, 0F);
         pike5.setTexSize(32, 16);
         pike5.mirror = true;
-        pike6 = new RendererModel(this, 20, 7);
+        pike6 = new ModelRenderer(this, 20, 7);
         pike6.addBox(-0.5F, 0F, -2.5F, 1, 1, 1);
         pike6.setPos(0F, -6F, 0F);
         pike6.setTexSize(32, 16);
         pike6.mirror = true;
-        pike7 = new RendererModel(this, 20, 7);
+        pike7 = new ModelRenderer(this, 20, 7);
         pike7.addBox(1.5F, 0F, -0.5F, 1, 1, 1);
         pike7.setPos(0F, -6F, 0F);
         pike7.setTexSize(32, 16);
         pike7.mirror = true;
-        pike8 = new RendererModel(this, 20, 7);
+        pike8 = new ModelRenderer(this, 20, 7);
         pike8.addBox(1.5F, 0F, -2.5F, 1, 1, 1);
         pike8.setPos(0F, -6F, 0F);
         pike8.setTexSize(32, 16);
         pike8.mirror = true;
     }
     
-    public void render(float f5) {
-        body1.render(f5);
-        body2.render(f5);
-        body3.render(f5);
-        pike1.render(f5);
-        pike2.render(f5);
-        pike3.render(f5);
-        pike4.render(f5);
-        pike5.render(f5);
-        pike6.render(f5);
-        pike7.render(f5);
-        pike8.render(f5);
+    public void renderToBuffer(MatrixStack pMatrixStack, IVertexBuilder pBuffer, int pPackedLight, int pPackedOverlay, float pRed, float pGreen, float pBlue, float pAlpha) {
+        body1.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        body2.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        body3.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        pike1.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        pike2.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        pike3.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        pike4.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        pike5.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        pike6.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        pike7.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        pike8.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
     }
 }
--- src/main/java/com/vanym/paniclecraft/client/renderer/model/ModelPortableWorkbench.java
+++ src/main/java/com/vanym/paniclecraft/client/renderer/model/ModelPortableWorkbench.java
@@ -1,61 +1,66 @@
 package com.vanym.paniclecraft.client.renderer.model;
 
-import net.minecraft.client.renderer.entity.model.RendererModel;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.model.Model;
+import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class ModelPortableWorkbench extends Model {
-    RendererModel Shape1;
-    RendererModel Shape2;
-    RendererModel Shape3;
-    RendererModel Shape4;
-    RendererModel Shape5;
-    RendererModel Shape6;
-    RendererModel Shape7;
+    ModelRenderer Shape1;
+    ModelRenderer Shape2;
+    ModelRenderer Shape3;
+    ModelRenderer Shape4;
+    ModelRenderer Shape5;
+    ModelRenderer Shape6;
+    ModelRenderer Shape7;
     
     public ModelPortableWorkbench() {
+        super(RenderType::entitySolid);
         texWidth = 64;
         texHeight = 32;
         
-        Shape1 = new RendererModel(this, 0, 0);
+        Shape1 = new ModelRenderer(this, 0, 0);
         Shape1.addBox(-8F, -2F, -8F, 16, 3, 16);
         Shape1.setPos(0F, 0F, 0F);
         Shape1.setTexSize(64, 32);
         Shape1.mirror = true;
         setRotation(Shape1, 0F, 0F, 0F);
-        Shape2 = new RendererModel(this, 0, 19);
+        Shape2 = new ModelRenderer(this, 0, 19);
         Shape2.addBox(-9F, -1F, 5F, 1, 1, 1);
         Shape2.setPos(0F, 0F, 0F);
         Shape2.setTexSize(64, 32);
         Shape2.mirror = true;
         setRotation(Shape2, 0F, 0F, 0F);
-        Shape3 = new RendererModel(this, 0, 21);
+        Shape3 = new ModelRenderer(this, 0, 21);
         Shape3.addBox(-9F, -1F, -5F, 1, 1, 1);
         Shape3.setPos(0F, 0F, 0F);
         Shape3.setTexSize(64, 32);
         Shape3.mirror = true;
         setRotation(Shape3, 0F, 0F, 0F);
-        Shape4 = new RendererModel(this, 40, 19);
+        Shape4 = new ModelRenderer(this, 40, 19);
         Shape4.addBox(-10F, -1F, -5F, 1, 1, 11);
         Shape4.setPos(0F, 0F, 0F);
         Shape4.setTexSize(64, 32);
         Shape4.mirror = true;
         setRotation(Shape4, 0F, 0F, 0F);
-        Shape5 = new RendererModel(this, 0, 23);
+        Shape5 = new ModelRenderer(this, 0, 23);
         Shape5.addBox(8F, -1F, 5F, 1, 1, 1);
         Shape5.setPos(0F, 0F, 0F);
         Shape5.setTexSize(64, 32);
         Shape5.mirror = true;
         setRotation(Shape5, 0F, 0F, 0F);
-        Shape6 = new RendererModel(this, 0, 25);
+        Shape6 = new ModelRenderer(this, 0, 25);
         Shape6.addBox(8F, -1F, -5F, 1, 1, 1);
         Shape6.setPos(0F, 0F, 0F);
         Shape6.setTexSize(64, 32);
         Shape6.mirror = true;
         setRotation(Shape6, 0F, 0F, 0F);
-        Shape7 = new RendererModel(this, 16, 19);
+        Shape7 = new ModelRenderer(this, 16, 19);
         Shape7.addBox(9F, -1F, -5F, 1, 1, 11);
         Shape7.setPos(0F, 0F, 0F);
         Shape7.setTexSize(64, 32);
@@ -63,17 +68,17 @@ public class ModelPortableWorkbench extends Model {
         setRotation(Shape7, 0F, 0F, 0F);
     }
     
-    public void render(float f5) {
-        Shape1.render(f5);
-        Shape2.render(f5);
-        Shape3.render(f5);
-        Shape4.render(f5);
-        Shape5.render(f5);
-        Shape6.render(f5);
-        Shape7.render(f5);
+    public void renderToBuffer(MatrixStack pMatrixStack, IVertexBuilder pBuffer, int pPackedLight, int pPackedOverlay, float pRed, float pGreen, float pBlue, float pAlpha) {
+        Shape1.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        Shape2.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        Shape3.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        Shape4.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        Shape5.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        Shape6.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+        Shape7.render(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
     }
     
-    private void setRotation(RendererModel model, float x, float y, float z) {
+    private void setRotation(ModelRenderer model, float x, float y, float z) {
         model.xRot = x;
         model.yRot = y;
         model.zRot = z;
