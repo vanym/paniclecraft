ddea3a159e892f0974986657f940135bb89fa550
VanyM <vanymdev@gmail.com>
2025-10-03 01:33:37 +0400

    add missing SideOnly to ClientCommandMod3, ClientCommandVersion 2c4542a2

--- src/main/java/com/vanym/paniclecraft/client/command/ClientCommandMod3.java
+++ src/main/java/com/vanym/paniclecraft/client/command/ClientCommandMod3.java
@@ -2,6 +2,10 @@ package com.vanym.paniclecraft.client.command;
 
 import com.vanym.paniclecraft.command.CommandMod3;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
 public class ClientCommandMod3 extends CommandMod3 {
     
     public static final String NAME = CommandMod3.NAME + "client";
--- src/main/java/com/vanym/paniclecraft/client/command/ClientCommandVersion.java
+++ src/main/java/com/vanym/paniclecraft/client/command/ClientCommandVersion.java
@@ -2,8 +2,11 @@ package com.vanym.paniclecraft.client.command;
 
 import com.vanym.paniclecraft.command.CommandVersion;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.command.ICommandSender;
 
+@SideOnly(Side.CLIENT)
 public class ClientCommandVersion extends CommandVersion {
     
     @Override
